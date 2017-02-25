94205d61340b7d370a58740a60cd7bb79bd3b681
osmaraortiz <osmaraeditho@gmail.com>
2017-02-25 12:22:16 -0700

    hola

---
+++ app/src/main/java/com/example/osmara/helloworld/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.osmara.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.osmara.helloworld.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
